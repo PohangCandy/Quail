7335dddb5e90ca0676c3db1a7aa72d2c4122abf0
PohangCandy <rlcks777@naver.com>
2024-10-08 20:51:52 +0900

    making textui to objects

--- OOP2/OOP2.cpp
+++ OOP2/OOP2.cpp
@@ -16,37 +16,39 @@
 #include "Enemy.h"
 #include "Bullet.h"
 #include "TextUI.h"
-#include "InputManager.h"
 
 using namespace std;
 
 int main()
 {
-	Borland::Initialize();
-	InputManager inputManager;
-
+	//싱글톤으로 작성된 Canvas 클래스
+	//캔버스의 인스턴스가 없다면 생성하고, 있다면 해당 객체를 가지고 온다.
 	auto canvas = Canvas::GetInstance();
+	//GameObject의 PendingObjects에 생성하고 싶은 게임 오브젝트 자식객체 담아두기
+	//Palyer는 모양, 위치, hp 정보 기본 생성자 인자로 가지고
+	//Enemy는 거기에 추가로 프레임당 데미지 정보를 가진다.
 	GameObject::Init(30);
-
-	Borland::GotoXY(1, 1);
-
-	cout << "hello world";
-
-	int x = Borland::WhereX();
-	int y = Borland::WhereY();
 
 	bool exit_flag{ false };
 
 
 	while (exit_flag == false) { // if player is alive
 
+		//캔버스 인스턴스에 있는 모든 내용을 ''으로 초기화 시킨다. 
+		//마지막 캐릭터 배열의 값에 널 값을 넣어 문자열로 취급할 수 있게 만든다.
+		//화면을 지우고 다시 그릴 수 있도록 준비한다.
 		canvas->clear();
 
-		/* process game logic for each game object */
+		//1.모든 Objects객체와 children의 update()함수를 호출한다.
+		// 플레이어, 적, 총알은 겹치는 상대 객체가 데미지를 받도록 한다.
+		// TextUI는 부모 객체의 남은 hp를 일정 시간동안 표시한다.
+		//2.Objects중에 isAlive()가 false인 객체를 모두 해제시키고 지운다.
+		//3.남아있는 PendingObjects를 Objects에 담는다.
 		GameObject::Update();
+		//프레임을 증가시킨다.
 		canvas->update();
 
-		/* draw game objects */
+		//
 		GameObject::Draw();
 
 		/* process input event. */
@@ -68,6 +70,8 @@ int main()
 				break;
 			}
 		}
+		//screen에 작성된 내용을 그리고
+		//정해진 frame만큼 sleep한다.
 		canvas->render();
 
 	} /* end of while(1) */
--- OOP2/Utils.cpp
+++ OOP2/Utils.cpp
@@ -1,7 +1,5 @@
 #include "Utils.h"
 
-int Debug::nDebugLine;
-char Debug::whiteSpaces[80] = { '\0' };
 
 bool isOverlap(int src_start, int src_end, int target_start, int target_end)
 {
